67e7ae8cc780392b25f8fbbf0eac78dcae4cfbb1
V Olek MP <olek.mp23@gmail.com>
2023-09-03 18:19:10 -0500

    Add stores module

---
+++ stores/internal/application/commands/remove_product.go
@@ -0,0 +1,28 @@
+package commands
+
+import (
+	"context"
+	"github.com/Sraik25/event-driven-architecture/stores/internal/domain"
+)
+
+type (
+	RemoveProduct struct {
+		ID string
+	}
+
+	RemoveProductHandler struct {
+		stores   domain.StoreRepository
+		products domain.ProductRepository
+	}
+)
+
+func NewRemoveProductHandler(stores domain.StoreRepository, products domain.ProductRepository) RemoveProductHandler {
+	return RemoveProductHandler{
+		stores:   stores,
+		products: products,
+	}
+}
+
+func (h RemoveProductHandler) RemoveProduct(ctx context.Context, cmd RemoveProduct) error {
+	return h.products.RemoveProduct(ctx, cmd.ID)
+}
